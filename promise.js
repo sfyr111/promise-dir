828062cf35aa70acf4b0e542755fe9c59d49e086
sfyr111 <sfyr111@163.com>
2018-02-10 17:23:39 +0800

    promise

---
+++ promise.js
@@ -0,0 +1,135 @@
+function Promise(fn) {
+  var value,
+      state = 'pending',
+      deferred = null // 待执行
+
+  function resolve(newValue) {
+    try {
+      if (newValue && typeof newValue.then === 'function') {
+        newValue.then(resolve, reject)
+        return
+      }
+      state = 'resolved'
+      value = newValue
+
+      deferred && handle(deferred)
+    } catch (e) {
+      reject(e)
+    }
+  }
+
+  function reject(reason) {
+    state = 'rejected'
+    value = reason
+
+    deferred && handle(deferred)
+  }
+
+  function handle(handler) {
+    if (state === 'pending') {
+      deferred = handler
+      return
+    }
+
+    var handlerCallback
+
+    handlerCallback = state === 'resolved' ? handler.onResolved : handler.onRejected
+
+    if (!handlerCallback) {
+      state === 'resolved'
+        ? handler.resolve(value)
+        : handler.reject(value)
+      return
+    }
+
+    var ret
+    try {
+      ret = handlerCallback(value)
+      handler.resolve(ret)
+    } catch (e) {
+      handler.reject(e)
+    }
+  }
+
+  this.then = function(onResolved, onRejected) {
+    return new Promise(function (resolve, reject) {
+      handle({
+        onResolved: onResolved,
+        onRejected: onRejected,
+        resolve: resolve,
+        reject: reject
+      })
+    })
+  }
+
+  fn(resolve, reject)
+}
+
+/**
+ * @feature 正确处理
+ * @returns {Promise}
+ */
+function getSuccess() {
+  return new Promise(function(resolve) {
+    setTimeout(function(resp) {
+      resp = { status: 200, data: 'resp', error: null }
+      resolve(resp.data)
+    }, 800)
+  })
+}
+
+getSuccess().then(function(data) {
+    console.log(data)
+  })
+
+/**
+ * @feature 错误处理
+ * @returns {Promise}
+ */
+// function getError() {
+//   return new Promise(function(resolve, reject) {
+//     setTimeout(function(resp) {
+//       resp = { status: 404, data: 'error', error: 'not found!!' }
+//       if (resp.error) {
+//         reject(resp.error)
+//       }
+//       resolve(resp.data)
+//     })
+//   })
+// }
+//
+// getError().then(function(data) {
+//   console.log(data)
+// }, function(error) {
+//   console.log(error)
+// })
+
+/**
+ * 链式调用
+ */
+// new Promise ((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(1)
+//   }, 500)
+// }).then((val) => {
+//   console.log(val)
+//   return new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(2)
+//     }, 500)
+//   })
+// }).then((val) => {
+//   console.log(val)
+//   return new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(3)
+//     }, 500)
+//   })
+// }).then((val) => {
+//   console.log(val)
+//   return new Promise ((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(5)
+//     }, 500)
+//   })
+// })
